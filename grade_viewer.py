441fc00f425eb7ea6d81d1a521cdb079a82d9614
alexthejpeg <alexphan85120@gmail.com>
2020-04-21 12:59:26 -0400

    Initial commit

---
+++ grade_viewer.py
@@ -0,0 +1,21 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+from window import Ui_MainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    app.setApplicationName("Grade Viewer")
+
+    window = MainWindow()
+    app.exec_()
